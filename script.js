ade0da333ab505f0e69257c12e60d6e21e492ab0
Priyanshi <priyanshi.chauhan2017@vitstudent.ac.in>
2020-07-18 19:52:49 -0700

    buttons added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
     const visual = document.querySelector('.visual');
-    const colors =[
+    const colors = [
         "lightcoral",
         "lightgrey",
         "lightsalmon",
@@ -16,7 +16,7 @@ window.addEventListener('load', () => {
 
 
     pads.forEach((pad, index) => {
-        pad.addEventListener('click', function(){
+        pad.addEventListener('click', function () {
             sounds[index].currentTime = 0;
             sounds[index].play();
 
@@ -30,8 +30,30 @@ window.addEventListener('load', () => {
         visual.appendChild(bubble);
         bubble.style.backgroundColor = colors[index];
         bubble.style.animation = 'jump 2s ease';
-        bubble.addEventListener('animationend', function(){
+        bubble.addEventListener('animationend', function () {
             visual.removeChild(this);
         });
     };
+
+
+
 });
+
+$(function () {
+    $(".btn").on("click", function (e) {
+        e.preventDefault();
+        $(".content").hide();
+        $("#" + this.id + "div").show();
+    });
+});
+
+
+function pianobg() {
+    document.body.style.backgroundImage = "url(https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf.jpg)";
+}
+
+
+function guitarbg() {
+    document.body.style.backgroundImage = "url(https://www.escapehimalaya.com/wp-content/uploads/2017/07/mountain-1209497_1920-1280x600.jpg)";
+}
+
